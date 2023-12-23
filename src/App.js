c5b8bdc7a1868fb9211308cf89becace59c9061a
MuhammedRasalAnsar47 <rasalansar47@gmail.com>
2023-12-23 11:24:52 +0530

    Finalfinal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+  const [currentDay, setCurrentDay] = useState('');
+
+  useEffect(() => {
+    const today = new Date();
+    const options = { weekday: 'long' };
+    const day = new Intl.DateTimeFormat('en-US', options).format(today);
+    setCurrentDay(day);
+  }, []);
+
+  const handleDelete = (id) => {
+    setToDos((prevToDos) => prevToDos.filter((obj) => obj.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>{`Whoop, it's ${currentDay} 🌝 ☕`}</h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setToDos([
+              ...toDos,
+              { id: Date.now(), text: toDo, status: false },
+            ])
+          }
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input
+                onChange={(e) =>
+                  setToDos((prevToDos) =>
+                    prevToDos.map((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked;
+                      }
+                      return obj2;
+                    })
+                  )
+                }
+                checked={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i
+                onClick={() => handleDelete(obj.id)}
+                className="fas fa-times"
+              ></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
